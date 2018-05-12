9b505e86a1558967845fa46ef977b92a981c32f9
turkana78 <turkanayar@gmail.com>
2018-05-12 20:13:11 +0300

    no message

--- server.js
+++ server.js
@@ -401,8 +401,8 @@ app.post('/api/getMessage/witai/:collectionName', cors(), function(req, res){
           client.get('https://api.wit.ai/message?q=' + encodeURIComponent(searchedItem), wit, function(response){
             if(response.entities.day && response.entities.month && response.entities.year){
               console.log("Tarih bilgisi tam ");
-              var obj = JSON.parse(response.entities);
-              var day = JSON.stringify(obj.day.value);
+              var obj = JSON.parse(response.entities.day);
+              var day = JSON.stringify(obj.value);
               var month = JSON.stringify(response.entities.month);
               var year = JSON.stringify(response.entities.year);
               res.send({text :  'İzin başlangıç tarihiniz ' + day + '.' + month + '.' + year + 'olarak alınmıştır.'});
