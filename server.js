cbb25bf33a70529bba637fc5551abae52d7d3b84
turkana78 <turkanayar@gmail.com>
2018-06-04 21:29:36 +0300

    no message

--- server.js
+++ server.js
@@ -430,7 +430,11 @@ app.post('/api/getMessage/witai/:collectionName', cors(), function(req, res){
             }
             else if(subjectLocal == "izin bitiş onay" && zero){
               console.log("subjectLocal : " + subjectLocal);
-              res.send({text :  req.body.obj.message.text + ' Onaylıyor musun? (Evet / Hayır)'});
+              res.send({text : req.body.obj.message.text + ' Onaylıyor musun? (Evet / Hayır)'});
+            }
+            else if(subjectLocal == "mazeret izin çeşitleri" && response.entities.mazarettip){
+              console.log("subjectLocal : " + subjectLocal);
+              res.send({text :  "Mazeret tipini " + response.entities.mazarettip[0].value + ' olarak anluyorum. Onaylıyor musun? (Evet / Hayır)'});
             }
             else if(response.entities && response.entities.intent && response.entities.intent.length > 0){
                 console.log("Subject var Intent varsa.");
