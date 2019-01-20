32b2998db2a17c38239bf0bcc705ba3ca51aeaaf
turkana78 <turkanayar@gmail.com>
2019-01-20 18:49:13 +0300

    test

--- server.js
+++ server.js
@@ -76,6 +76,7 @@ app.use(function (req, res, next){
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('P3P: CP',"IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA");
     next();
 });
 
